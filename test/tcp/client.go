6134fd231d9478c4def9171a4a6485e61ce166e5
codermyj <lightmyj@163.com>
2022-10-25 16:57:31 +0800

    修改

---
+++ test/tcp/client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:20000")
+	fmt.Printf("客户端: %T\n", conn)
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+	defer conn.Close()
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := inputReader.ReadString('\n')
+		inputInfo := strings.Trim(input, "\r\n")
+		if strings.ToUpper(inputInfo) == "Q" {
+			return
+		}
+		_, err := conn.Write([]byte(inputInfo))
+		if err != nil {
+			return
+		}
+		buf := [512]byte{}
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("接收数据失败, err: ", err)
+			return
+		}
+		fmt.Println("客户端接收服务端发送的数据: ", string(buf[:n]))
+	}
+}
