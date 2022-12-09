b640c5ae9bc6c6511c61707f20537cd8ba842c87
codermyj <lightmyj@163.com>
2022-12-09 17:59:12 +0800

    重写底层存储架构，以二进制方式存储数据

--- src/entry/entry.go
+++ src/entry/entry.go
@@ -2,8 +2,18 @@ package entry
 
 import (
 	"encoding/binary"
+	"errors"
 	"github.com/duke-git/lancet/v2/convertor"
+	"os"
 	"strconv"
+)
+
+const USIZE_LEN = strconv.IntSize / 8
+const ENTRY_HEAD_LEN = USIZE_LEN*2 + 1
+
+const (
+	PUT = 1
+	DEL = 2
 )
 
 type Entry struct {
@@ -14,16 +24,17 @@ type Entry struct {
 	Value    string //值
 }
 
-const (
-	PUT = 1
-	DEL = 2
-)
-
-const UsizeLen = strconv.IntSize / 8
-const EntryHeadLen = UsizeLen*2 + 1
+func NewEntry(key string, value string, kind uint) *Entry {
+	return &Entry{
+		uint(len(key)),
+		uint(len(value)),
+		kind,
+		key,
+		value}
+}
 
 func (entry *Entry) size() uint {
-	return EntryHeadLen + entry.KeyLen + entry.ValueLen
+	return ENTRY_HEAD_LEN + entry.KeyLen + entry.ValueLen
 }
 
 // 对数据进行二进制编码
@@ -38,7 +49,7 @@ func (entry *Entry) encode() []byte {
 	buf = append(buf, bufValueLen...)
 
 	bufKind, _ := convertor.ToBytes(entry.Kind)
-	buf = append(buf, bufKind[UsizeLen-1])
+	buf = append(buf, bufKind[USIZE_LEN-1])
 
 	buf = append(buf, []byte(entry.Key)...)
 
@@ -47,14 +58,79 @@ func (entry *Entry) encode() []byte {
 	return buf
 }
 
-// 对二进制数据进行解码
+// 对二进制数据进行Entry头部解码
 func decode(buf []byte) *Entry {
 
-	KeyLen := uint(binary.BigEndian.Uint64(buf[0:UsizeLen]))
-	ValueLen := uint(binary.BigEndian.Uint64(buf[UsizeLen : UsizeLen*2]))
-	Kind := uint(binary.BigEndian.Uint64(buf[UsizeLen*2 : UsizeLen*2+1]))
-	Key := string(buf[UsizeLen*2+1 : UsizeLen*2+1+KeyLen])
-	Value := string(buf[UsizeLen*2+1+KeyLen : UsizeLen*2+1+KeyLen+ValueLen])
+	KeyLen := uint(binary.BigEndian.Uint64(buf[0:USIZE_LEN]))
+	ValueLen := uint(binary.BigEndian.Uint64(buf[USIZE_LEN : USIZE_LEN*2]))
+	Kind := uint(binary.BigEndian.Uint64(buf[USIZE_LEN*2 : USIZE_LEN*2+1]))
+	//Key := string(buf[USIZE_LEN*2+1 : USIZE_LEN*2+1+KeyLen])
+	//Value := string(buf[USIZE_LEN*2+1+KeyLen : USIZE_LEN*2+1+KeyLen+ValueLen])
 
-	return &Entry{KeyLen, ValueLen, Kind, Key, Value}
+	return &Entry{KeyLen, ValueLen, Kind, "", ""}
+}
+
+type Storage interface {
+	get(key string) (string, error)
+	put(key string, val string) error
+	remove(key string) error
+}
+
+type SimplifiedBitcask struct {
+	dataPathBuf    string
+	reader         os.File
+	writer         os.File
+	index          map[string]uint64
+	pendingCompact uint64
+}
+
+func (s *SimplifiedBitcask) read(key string) (*Entry, error) {
+	pos, ok := s.index[key]
+	var err error
+	if ok {
+		return s.readAt(pos)
+	}
+	err = errors.New("the key not found: " + key)
+	return nil, err
+}
+
+func (s *SimplifiedBitcask) readAt(offset uint64) (*Entry, error) {
+	//解码Entry头
+	bufHead := make([]byte, ENTRY_HEAD_LEN)
+	_, err := s.reader.ReadAt(bufHead, int64(offset))
+	if err != nil {
+		return nil, err
+	}
+	entry := decode(bufHead)
+
+	pos := offset + ENTRY_HEAD_LEN
+
+	//解码Key
+	bufKey := make([]byte, entry.KeyLen)
+	_, err = s.reader.ReadAt(bufKey, int64(pos))
+	if err != nil {
+		return nil, err
+	}
+	entry.Key = string(bufKey)
+
+	pos += uint64(entry.KeyLen)
+
+	//解码Value
+	bufValue := make([]byte, entry.ValueLen)
+	_, err = s.reader.ReadAt(bufValue, int64(pos))
+	if err != nil {
+		return nil, err
+	}
+	entry.Value = string(bufValue)
+
+	return entry, nil
+}
+
+func (s *SimplifiedBitcask) get(key string) (string, error) {
+	entry, err := s.read(key)
+	return entry.Value, err
+}
+
+func (s *SimplifiedBitcask) put(key string, val string) {
+
 }
